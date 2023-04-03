3ffd589306670fc6809fc99046fe24a0bba8c0ba
Undie92 <Undie92@hotmail.com>
2023-04-03 17:58:09 +0000

    Added styling via import from css

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 
 const Sidebar = () => {
 
-    return <div>
-        <a>hej</a>
-        <a>hej</a>
-        <a>hej</a>
+    return <div className = {css.sidebar}>
+        <a>My Photos</a>
+        <a>My Illustrations</a>
+        <a>My Paintings</a>
     </div>
 }
 
